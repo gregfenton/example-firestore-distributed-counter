6dd863f09be4752e4cf38ff8705b640abfe6a9a2
Greg Fenton <greg.fenton@gmail.com>
2021-09-07 22:03:17 -0400

    simplify pubsub message sending

--- functions/fs/distPubSub/onCreate.f.js
+++ functions/fs/distPubSub/onCreate.f.js
@@ -4,85 +4,84 @@ const CONSTS = require('../../constants');
 const projectId = 'example-fs-distributed-counter';
 const topicName = 'distPubSub_topic';
 
-// Imports the Google Cloud client library. v1 is for the lower level
-// proto access.
-const { v1 } = require('@google-cloud/pubsub');
-
-// Creates a publisher client.
-const publisherClient = new v1.PublisherClient({
-  // optional auth parameters
-});
-
-const publishWithRetry = async (docId) => {
-  const formattedTopic = publisherClient.projectTopicPath(projectId, topicName);
-
-  const data = JSON.stringify({ docId: docId });
-
-  // Publishes the message as JSON object
-  const dataBuffer = Buffer.from(data);
-  const messagesElement = {
-    data: dataBuffer,
-  };
-  const messages = [messagesElement];
-
-  // Build the request
-  const request = {
-    topic: formattedTopic,
-    messages: messages,
-  };
-
-  // Retry settings control how the publisher handles retryable failures.
-  // Default values are shown.
-  // The `retryCodes` array determines which GRPC errors trigger a retry.
-  // The `backoffSettings` object lets you specify the behaviour of retries.
-  const retrySettings = {
-    retryCodes: [
-      10, // 'ABORTED'
-      1, // 'CANCELLED',
-      4, // 'DEADLINE_EXCEEDED'
-      13, // 'INTERNAL'
-      8, // 'RESOURCE_EXHAUSTED'
-      14, // 'UNAVAILABLE'
-      2, // 'UNKNOWN'
-    ],
-    backoffSettings: {
-      // The initial delay time, in milliseconds, between the completion
-      // of the first failed request and the first retry request.
-      initialRetryDelayMillis: 100,
-      // The multiplier to increase the delay time between the completion
-      // of failed requests, and the initiation of the subsequent retry.
-      retryDelayMultiplier: 1.3,
-      // The maximum delay time, in milliseconds, between requests.
-      // When this value is reached, retryDelayMultiplier is no longer used.
-      maxRetryDelayMillis: 60000,
-      // The initial timeout parameter to the request.
-      initialRpcTimeoutMillis: 5000,
-      // The multiplier to increase the timeout between failed requests.
-      rpcTimeoutMultiplier: 1.0,
-      // The maximum timeout, in milliseconds, for a request. When reached,
-      // rpcTimeoutMultiplier will no longer be used to increase the timeout.
-      maxRpcTimeoutMillis: 600000,
-      // The total time, in milliseconds, from when the initial request is sent,
-      // after which an error will be returned, regardless of retry attempts.
-      totalTimeoutMillis: 600000,
-    },
-  };
-
-  const [response] = await publisherClient.publish(request, {
-    retry: retrySettings,
-  });
-  console.log(
-    `publishWithRetry(): published(${response.messageIds}) for docId(${docId})`
-  );
-};
-
 // Cloud Function to increment the document field after creation.
 export default functions
   .runWith({ memory: '1GB', timeoutSeconds: 300 })
   .firestore.document(`${CONSTS.DISTPUBSUB_COLLECTION_NAME}/{docId}`)
   .onCreate((snap, context) => {
-    console.log(`distPubSub: onCreate(${snap.ref.id})`);
-    return publishWithRetry(snap.ref.id).catch((e) => {
-      console.error('distPubSub: publishWithRetry() FAILED!!', e);
-    });
+    console.log(
+      'distPubSubOnCreate(): PUB envs are: ' +
+        Object.keys(process.env).filter((x) => x.startsWith('PUB'))
+    );
+
+    console.log(`distPubSubOnCreate(): onCreate(${snap.ref.id})`);
+    console.log(
+      'distPubSubOnCreate(): PUBSUB_EMULATOR_HOST is (' +
+        `${process.env.PUBSUB_EMULATOR_HOST})`
+    );
+    console.log(
+      'distPubSubOnCreate(): PUBSUB_PROJECT_ID is (' +
+        `${process.env.PUBSUB_PROJECT_ID})`
+    );
+    console.log(
+      `distPubSubOnCreate(): GCLOUD_PROJECT is (${process.env.GCLOUD_PROJECT})`
+    );
+
+    try {
+      // Imports the Google Cloud client library. v1 is for the lower level
+      // proto access.
+      const { v1 } = require('@google-cloud/pubsub');
+
+      // Creates a publisher client.
+      const publisherClient = new v1.PublisherClient({
+        projectId: process.env.GCLOUD_PROJECT,
+        port: 8085,
+        api: 'localhost',
+        // optional auth parameters
+      });
+
+      const formattedTopic = publisherClient.projectTopicPath(
+        projectId,
+        topicName
+      );
+
+      const data = JSON.stringify({ docId: snap.id });
+
+      // Publishes the message as JSON object
+      const dataBuffer = Buffer.from(data);
+      const messagesElement = {
+        data: dataBuffer,
+      };
+      const messages = [messagesElement];
+
+      // Build the request
+      const request = {
+        topic: formattedTopic,
+        messages: messages,
+      };
+
+      console.log('CALLING .publish()');
+      return publisherClient
+        .publish(request)
+        .then(([responses]) => {
+          console.log(
+            `distPubSubOnCreate(): responses is ${JSON.stringify(
+              responses,
+              null,
+              2
+            )}`
+          );
+          console.log(
+            `distPubSubOnCreate(): published(${responses.messageIds}) ` +
+              `for docId(${snap.id})`
+          );
+        })
+        .catch((ex) => {
+          console.error(`distPubSubOnCreate(): ERROR: ${ex.message}`);
+          throw ex; // be sure to fail the function
+        });
+    } catch (ex) {
+      console.log(`EXCEPTION: ${ex.message}`);
+      throw ex;
+    }
   });
